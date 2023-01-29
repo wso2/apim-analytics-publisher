a6013f0e17e71c98bf36b6f3c52f5dabf35686ac
SarithRavI <imaduwagesarith932@gmail.com>
2023-01-29 20:25:22 +0000

    Added moesif specific utils.

---
+++ component/publisher-client/src/main/java/org/wso2/am/analytics/publisher/reporter/moesif/util/MoesifMicroserviceConstants.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.am.analytics.publisher.reporter.moesif.util;
+
+/**
+ * Class for constants related to external Moesif microservice.
+ */
+public class MoesifMicroserviceConstants {
+    public static final String DETAIL_URL = "http://microservice:port/moesif_key/";
+    public static final String LIST_URL = "http://microservice:port/moesif_key";
+    public static final String GA_USERNAME_CONFIG_KEY = "gaAuthUsername";
+    public static final String GA_PWD_CONFIG_KEY = "gaAuthPwd";
+    public static final String CONTENT_TYPE = "application/json";
+    public static final String QUERY_PARAM = "org_id";
+    public static final int NUM_RETRY_ATTEMPTS = 3;
+    public static final long TIME_TO_WAIT = 10000;
+    public static final int NUM_RETRY_ATTEMPTS_PUBLISH = 3;
+    public static final long TIME_TO_WAIT_PUBLISH = 10000;
+    public static final int REQUEST_READ_TIMEOUT = 10000;
+
+}
